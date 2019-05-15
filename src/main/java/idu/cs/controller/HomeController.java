6af4e010ad435800b39993275c467e9683b75996
JG <dbdpfk158@naver.com>
2019-05-15 17:13:35 +0900

    2019-05-15

--- src/main/java/idu/cs/controller/HomeController.java
+++ src/main/java/idu/cs/controller/HomeController.java
@@ -5,10 +5,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
 import idu.cs.domain.User;
+import idu.cs.exception.ResourceNotFoundException;
 import idu.cs.repository.UserRepository;
 
 @Controller
@@ -16,10 +18,13 @@ public class HomeController {
 	@Autowired UserRepository userRepo; // Dependency Injection
 	
 	@GetMapping("/")
-	public String home(Model model) {
-		model.addAttribute("test", "인덕 컴소");
-		model.addAttribute("egy", "유응구");
+	public String home() {
 		return "index";
+	}
+	@GetMapping("/register")
+	public String getRegForm() {
+		
+		return "form";
 	}
 	@GetMapping("/users")
 	public String getAllUser(Model model) {
@@ -31,5 +36,14 @@ public class HomeController {
 		userRepo.save(user);
 		model.addAttribute("users", userRepo.findAll());
 		return "redirect:/users";
+	}
+	@GetMapping("/users/{id}")
+	public String getUserById(@PathVariable(value = "id") Long userId, Model model)
+			throws ResourceNotFoundException {
+		User user = userRepo.findById(userId).orElseThrow(() -> new ResourceNotFoundException("User not found for this id :: " + userId));
+		model.addAttribute("name", user.getName());
+		model.addAttribute("company", user.getCompany());
+		return "user";
+		//return ResponseEntity.ok().body(user);
 	}
 }
